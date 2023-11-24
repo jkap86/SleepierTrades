36e7c08d34a23cad100a7103aba1031030c82465
Jay Kapoor <jkap86@gmail.com>
2023-11-24 14:14:20 -0500

    -

--- client/src/modules/COMMON/services/hooks/useFetchLmPlayerShares.js
+++ client/src/modules/COMMON/services/hooks/useFetchLmPlayerShares.js
@@ -4,15 +4,15 @@ import { fetchLmPlayerShares } from "../../redux/actions";
 
 const useFetchLmPlayerShares = () => {
     const dispatch = useDispatch();
-    const { user_id, lmplayershares } = useSelector(state => state.user);
+    const { user_id, leagues, lmplayershares } = useSelector(state => state.user);
 
     console.log({lmplayershares})
 
     useEffect(() => {
-        if (user_id && !lmplayershares) {
+        if (user_id && leagues && !lmplayershares) {
             dispatch(fetchLmPlayerShares(user_id))
         }
-    }, [user_id, lmplayershares, dispatch])
+    }, [user_id, leagues, lmplayershares, dispatch])
 
 }
 
